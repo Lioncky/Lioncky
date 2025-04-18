07ec34b9fb55dfe6e9b53975a3ad09e534f1ce8b
lioncky <ksanl888@qq.com>
2025-04-19 05:27:15 +0800

    SSDT add 2600

---
+++ README.py
@@ -0,0 +1,801 @@
+# 原始函数列表（需要编号的）
+
+aaa="""
+NtAccessCheck
+NtWorkerFactoryWorkerReady
+NtAcceptConnectPort
+NtMapUserPhysicalPagesScatter
+NtWaitForSingleObject
+NtCallbackReturn
+NtReadFile
+NtDeviceIoControlFile
+NtWriteFile
+NtRemoveIoCompletion
+NtReleaseSemaphore
+NtReplyWaitReceivePort
+NtReplyPort
+NtSetInformationThread
+NtSetEvent
+NtClose
+NtQueryObject
+NtQueryInformationFile
+NtOpenKey
+NtEnumerateValueKey
+NtFindAtom
+NtQueryDefaultLocale
+NtQueryKey
+NtQueryValueKey
+NtAllocateVirtualMemory
+NtQueryInformationProcess
+NtWaitForMultipleObjects32
+NtWriteFileGather
+NtSetInformationProcess
+NtCreateKey
+NtFreeVirtualMemory
+NtImpersonateClientOfPort
+NtReleaseMutant
+NtQueryInformationToken
+NtRequestWaitReplyPort
+NtQueryVirtualMemory
+NtOpenThreadToken
+NtQueryInformationThread
+NtOpenProcess
+NtSetInformationFile
+NtMapViewOfSection
+NtAccessCheckAndAuditAlarm
+NtUnmapViewOfSection
+NtReplyWaitReceivePortEx
+NtTerminateProcess
+NtSetEventBoostPriority
+NtReadFileScatter
+NtOpenThreadTokenEx
+NtOpenProcessTokenEx
+NtQueryPerformanceCounter
+NtEnumerateKey
+NtOpenFile
+NtDelayExecution
+NtQueryDirectoryFile
+NtQuerySystemInformation
+NtOpenSection
+NtQueryTimer
+NtFsControlFile
+NtWriteVirtualMemory
+NtCloseObjectAuditAlarm
+NtDuplicateObject
+NtQueryAttributesFile
+NtClearEvent
+NtReadVirtualMemory
+NtOpenEvent
+NtAdjustPrivilegesToken
+NtDuplicateToken
+NtContinue
+NtQueryDefaultUILanguage
+NtQueueApcThread
+NtYieldExecution
+NtAddAtom
+NtCreateEvent
+NtQueryVolumeInformationFile
+NtCreateSection
+NtFlushBuffersFile
+NtApphelpCacheControl
+NtCreateProcessEx
+NtCreateThread
+NtIsProcessInJob
+NtProtectVirtualMemory
+NtQuerySection
+NtResumeThread
+NtTerminateThread
+NtReadRequestData
+NtCreateFile
+NtQueryEvent
+NtWriteRequestData
+NtOpenDirectoryObject
+NtAccessCheckByTypeAndAuditAlarm
+NtQuerySystemTime
+NtWaitForMultipleObjects
+NtSetInformationObject
+NtCancelIoFile
+NtTraceEvent
+NtPowerInformation
+NtSetValueKey
+NtCancelTimer
+NtSetTimer
+NtAcceptConnectPort
+NtAccessCheck
+NtAccessCheckByType
+NtAccessCheckByTypeResultList
+NtAccessCheckByTypeResultListAndAuditAlarm
+NtAccessCheckByTypeResultListAndAuditAlarmByHandle
+NtAcquireCrossVmMutant
+NtAcquireProcessActivityReference
+NtAddAtomEx
+NtAddBootEntry
+NtAddDriverEntry
+NtAdjustGroupsToken
+NtAdjustTokenClaimsAndDeviceGroups
+NtAlertResumeThread
+NtAlertThread
+NtAlertThreadByThreadId
+NtAllocateLocallyUniqueId
+NtAllocateReserveObject
+NtAllocateUserPhysicalPages
+NtAllocateUserPhysicalPagesEx
+NtAllocateUuids
+NtAllocateVirtualMemoryEx
+NtAlpcAcceptConnectPort
+NtAlpcCancelMessage
+NtAlpcConnectPort
+NtAlpcConnectPortEx
+NtAlpcCreatePort
+NtAlpcCreatePortSection
+NtAlpcCreateResourceReserve
+NtAlpcCreateSectionView
+NtAlpcCreateSecurityContext
+NtAlpcDeletePortSection
+NtAlpcDeleteResourceReserve
+NtAlpcDeleteSectionView
+NtAlpcDeleteSecurityContext
+NtAlpcDisconnectPort
+NtAlpcImpersonateClientContainerOfPort
+NtAlpcImpersonateClientOfPort
+NtAlpcOpenSenderProcess
+NtAlpcOpenSenderThread
+NtAlpcQueryInformation
+NtAlpcQueryInformationMessage
+NtAlpcRevokeSecurityContext
+NtAlpcSendWaitReceivePort
+NtAlpcSetInformation
+NtAreMappedFilesTheSame
+NtAssignProcessToJobObject
+NtAssociateWaitCompletionPacket
+NtCallEnclave
+NtCancelIoFileEx
+NtCancelSynchronousIoFile
+NtCancelTimer2
+NtCancelWaitCompletionPacket
+NtChangeProcessState
+NtChangeThreadState
+NtCommitComplete
+NtCommitEnlistment
+NtCommitRegistryTransaction
+NtCommitTransaction
+NtCancelDeviceWakeupRequest
+NtCompactKeys
+NtCompareObjects
+NtCompareSigningLevels
+NtCompareTokens
+NtCompleteConnectPort
+NtCompressKey
+NtConnectPort
+NtContinueEx
+NtConvertBetweenAuxiliaryCounterAndPerformanceCounter
+NtCopyFileChunk
+NtCreateCpuPartition
+NtCreateCrossVmEvent
+NtCreateCrossVmMutant
+NtCreateDebugObject
+NtCreateDirectoryObject
+NtCreateDirectoryObjectEx
+NtCreateEnclave
+NtCreateEnlistment
+NtCreateEventPair
+NtCreateIRTimer
+NtCreateIoCompletion
+NtCreateIoRing
+NtCreateJobObject
+NtCreateJobSet
+NtCreateKeyTransacted
+NtCreateKeyedEvent
+NtCreateLowBoxToken
+NtCreateMailslotFile
+NtCreateMutant
+NtCreateNamedPipeFile
+NtCreatePagingFile
+NtCreatePartition
+NtCreatePort
+NtCreatePrivateNamespace
+NtCreateProcess
+NtCreateProcessStateChange
+NtCreateProfile
+NtCreateProfileEx
+NtCreateRegistryTransaction
+NtCreateResourceManager
+NtCreateSectionEx
+NtCreateSemaphore
+NtCreateSymbolicLinkObject
+NtCreateThreadEx
+NtCreateThreadStateChange
+NtCreateTimer
+NtCreateTimer2
+NtCreateToken
+NtCreateTokenEx
+NtCreateTransaction
+NtCreateTransactionManager
+NtCreateUserProcess
+NtCreateWaitCompletionPacket
+NtCreateWaitablePort
+NtCreateWnfStateName
+NtCreateWorkerFactory
+NtDebugActiveProcess
+NtDebugContinue
+NtDeleteAtom
+NtDeleteBootEntry
+NtDeleteDriverEntry
+NtDeleteFile
+NtDeleteKey
+NtDeleteObjectAuditAlarm
+NtDeletePrivateNamespace
+NtDeleteValueKey
+NtDeleteWnfStateData
+NtDeleteWnfStateName
+NtDirectGraphicsCall
+NtDisableLastKnownGood
+NtDisplayString
+NtDrawText
+NtEnableLastKnownGood
+NtEnumerateBootEntries
+NtEnumerateDriverEntries
+NtEnumerateSystemEnvironmentValuesEx
+NtEnumerateTransactionObject
+NtExtendSection
+NtFilterBootOption
+NtFilterToken
+NtFilterTokenEx
+NtFlushBuffersFileEx
+NtFlushInstallUILanguage
+NtFlushInstructionCache
+NtFlushKey
+NtFlushProcessWriteBuffers
+NtFlushVirtualMemory
+NtFlushWriteBuffer
+NtFreeUserPhysicalPages
+NtFreezeRegistry
+NtFreezeTransactions
+NtGetCachedSigningLevel
+NtGetCompleteWnfStateSubscription
+NtGetContextThread
+NtGetCurrentProcessorNumber
+NtGetCurrentProcessorNumberEx
+NtGetDevicePowerState
+NtGetMUIRegistryInfo
+NtGetNextProcess
+NtGetNextThread
+NtGetNlsSectionPtr
+NtGetNotificationResourceManager
+NtGetPlugPlayEvent
+NtGetWriteWatch
+NtImpersonateAnonymousToken
+NtImpersonateThread
+NtInitializeEnclave
+NtInitializeNlsFiles
+NtInitializeRegistry
+NtInitiatePowerAction
+NtIsSystemResumeAutomatic
+NtIsUILanguageComitted
+NtListenPort
+NtLoadDriver
+NtLoadEnclaveData
+NtLoadKey
+NtLoadKey2
+NtLoadKey3
+NtLoadKeyEx
+NtLockFile
+NtLockProductActivationKeys
+NtLockRegistryKey
+NtLockVirtualMemory
+NtMakePermanentObject
+NtMakeTemporaryObject
+NtManageHotPatch
+NtManagePartition
+NtMapCMFModule
+NtMapUserPhysicalPages
+NtMapViewOfSectionEx
+NtModifyBootEntry
+NtModifyDriverEntry
+NtNotifyChangeDirectoryFile
+NtNotifyChangeDirectoryFileEx
+NtNotifyChangeKey
+NtNotifyChangeMultipleKeys
+NtNotifyChangeSession
+NtOpenCpuPartition
+NtOpenEnlistment
+NtOpenEventPair
+NtOpenIoCompletion
+NtOpenJobObject
+NtOpenKeyEx
+NtOpenKeyTransacted
+NtOpenKeyTransactedEx
+NtOpenKeyedEvent
+NtOpenMutant
+NtOpenObjectAuditAlarm
+NtOpenPartition
+NtOpenPrivateNamespace
+NtOpenProcessToken
+NtOpenRegistryTransaction
+NtOpenResourceManager
+NtOpenSemaphore
+NtOpenSession
+NtOpenSymbolicLinkObject
+NtOpenThread
+NtOpenTimer
+NtOpenTransaction
+NtOpenTransactionManager
+NtPlugPlayControl
+NtPrePrepareComplete
+NtPrePrepareEnlistment
+NtPrepareComplete
+NtPrepareEnlistment
+NtPrivilegeCheck
+NtPrivilegeObjectAuditAlarm
+NtPrivilegedServiceAuditAlarm
+NtPropagationComplete
+NtPropagationFailed
+NtPssCaptureVaSpaceBulk
+NtPulseEvent
+NtQueryAuxiliaryCounterFrequency
+NtQueryBootEntryOrder
+NtQueryBootOptions
+NtQueryDebugFilterState
+NtQueryDirectoryFileEx
+NtQueryDirectoryObject
+NtQueryDriverEntryOrder
+NtQueryEaFile
+NtQueryFullAttributesFile
+NtQueryInformationAtom
+NtQueryInformationByName
+NtQueryInformationCpuPartition
+NtQueryInformationEnlistment
+NtQueryInformationJobObject
+NtQueryInformationPort
+NtQueryInformationResourceManager
+NtQueryInformationTransaction
+NtQueryInformationTransactionManager
+NtQueryInformationWorkerFactory
+NtQueryInstallUILanguage
+NtQueryIntervalProfile
+NtQueryIoCompletion
+NtQueryIoRingCapabilities
+NtQueryLicenseValue
+NtQueryMultipleValueKey
+NtQueryMutant
+NtQueryOpenSubKeys
+NtQueryOpenSubKeysEx
+NtQueryPortInformationProcess
+NtQueryQuotaInformationFile
+NtQuerySecurityAttributesToken
+NtQuerySecurityObject
+NtQuerySecurityPolicy
+NtQuerySemaphore
+NtQuerySymbolicLinkObject
+NtQuerySystemEnvironmentValue
+NtQuerySystemEnvironmentValueEx
+NtQuerySystemInformationEx
+NtQueryTimerResolution
+NtQueryWnfStateData
+NtQueryWnfStateNameInformation
+NtQueueApcThreadEx
+NtQueueApcThreadEx2
+NtRaiseException
+NtRaiseHardError
+NtReadOnlyEnlistment
+NtReadVirtualMemoryEx
+NtRecoverEnlistment
+NtRecoverResourceManager
+NtRecoverTransactionManager
+NtRegisterProtocolAddressInformation
+NtRegisterThreadTerminatePort
+NtReleaseKeyedEvent
+NtReleaseWorkerFactoryWorker
+NtRemoveIoCompletionEx
+NtRemoveProcessDebug
+NtRenameKey
+NtRenameTransactionManager
+NtReplaceKey
+NtReplacePartitionUnit
+NtReplyWaitReplyPort
+NtRequestDeviceWakeup
+NtRequestPort
+NtRequestWakeupLatency
+NtResetEvent
+NtResetWriteWatch
+NtRestoreKey
+NtResumeProcess
+NtRevertContainerImpersonation
+NtRollbackComplete
+NtRollbackEnlistment
+NtRollbackRegistryTransaction
+NtRollbackTransaction
+NtRollforwardTransactionManager
+NtSaveKey
+NtSaveKeyEx
+NtSaveMergedKeys
+NtSecureConnectPort
+NtSerializeBoot
+NtSetBootEntryOrder
+NtSetBootOptions
+NtSetCachedSigningLevel
+NtSetCachedSigningLevel2
+NtSetContextThread
+NtSetDebugFilterState
+NtSetDefaultHardErrorPort
+NtSetDefaultLocale
+NtSetDefaultUILanguage
+NtSetDriverEntryOrder
+NtSetEaFile
+NtSetHighEventPair
+NtSetHighWaitLowEventPair
+NtSetIRTimer
+NtSetInformationCpuPartition
+NtSetInformationDebugObject
+NtSetInformationEnlistment
+NtSetInformationIoRing
+NtSetInformationJobObject
+NtSetInformationKey
+NtSetInformationResourceManager
+NtSetInformationSymbolicLink
+NtSetInformationToken
+NtSetInformationTransaction
+NtSetInformationTransactionManager
+NtSetInformationVirtualMemory
+NtSetInformationWorkerFactory
+NtSetIntervalProfile
+NtSetIoCompletion
+NtSetIoCompletionEx
+NtSetLdtEntries
+NtSetLowEventPair
+NtSetLowWaitHighEventPair
+NtSetQuotaInformationFile
+NtSetSecurityObject
+NtSetSystemEnvironmentValue
+NtSetSystemEnvironmentValueEx
+NtSetSystemInformation
+NtSetSystemPowerState
+NtSetSystemTime
+NtSetThreadExecutionState
+NtSetTimer2
+NtSetTimerEx
+NtSetTimerResolution
+NtSetUuidSeed
+NtSetVolumeInformationFile
+NtSetWnfProcessNotificationEvent
+NtShutdownSystem
+NtShutdownWorkerFactory
+NtSinglePhaseReject
+NtStartProfile
+NtSignalAndWaitForSingleObject
+NtStartProfile
+NtStopProfile
+NtSubmitIoRing
+NtSubscribeWnfStateChange
+NtSuspendProcess
+NtSuspendThread
+NtSystemDebugControl
+NtTerminateEnclave
+NtTerminateJobObject
+NtTestAlert
+NtThawRegistry
+NtThawTransactions
+NtTraceControl
+NtTranslateFilePath
+NtUmsThreadYield
+NtUnloadDriver
+NtUnloadKey
+NtUnloadKey2
+NtUnloadKeyEx
+NtUnlockFile
+NtUnlockVirtualMemory
+NtUnmapViewOfSectionEx
+NtUnsubscribeWnfStateChange
+NtUpdateWnfStateData
+NtVdmControl
+NtWaitForAlertByThreadId
+NtWaitForDebugEvent
+NtWaitForKeyedEvent
+NtWaitForWorkViaWorkerFactory
+NtWaitHighEventPair
+NtWaitLowEventPair
+"""
+input_funcs = [line.strip() for line in aaa.strip().splitlines() if line.strip()]
+
+
+# 编号对照表（编号 + 函数名）
+numbered_funcs = """
+0x01 		 NtAccessCheck
+0x02 		 NtAccessCheckAndAuditAlarm
+0x03 		 NtAccessCheckByType
+0x04 		 NtAccessCheckByTypeAndAuditAlarm
+0x05 		 NtAccessCheckByTypeResultList
+0x06 		 NtAccessCheckByTypeResultListAndAuditAlarm
+0x07 		 NtAccessCheckByTypeResultListAndAuditAlarmByHandle
+0x08 		 NtAddAtom
+0x09 		 NtAddBootEntry
+0x0A 		 NtAdjustGroupsToken
+0x0B 		 NtAdjustPrivilegesToken
+0x0C 		 NtAlertResumeThread
+0x0D 		 NtAlertThread
+0x0E 		 NtAllocateLocallyUniqueId
+0x0F 		 NtAllocateUserPhysicalPages
+0x10 		 NtAllocateUuids
+0x11 		 NtAllocateVirtualMemory
+0x12 		 NtAreMappedFilesTheSame
+0x13 		 NtAssignProcessToJobObject
+0x14 		 NtCallbackReturn
+0x15 		 NtCancelDeviceWakeupRequest
+0x16 		 NtCancelIoFile
+0x17 		 NtCancelTimer
+0x18 		 NtClearEvent
+0x19 		 NtClose
+0x1A 		 NtCloseObjectAuditAlarm
+0x1B 		 NtCompactKeys
+0x1C 		 NtCompareTokens
+0x1D 		 NtCompleteConnectPort
+0x1E 		 NtCompressKey
+0x1F 		 NtConnectPort
+0x20 		 NtContinue
+0x21 		 NtCreateDebugObject
+0x22 		 NtCreateDirectoryObject
+0x23 		 NtCreateEvent
+0x24 		 NtCreateEventPair
+0x25 		 NtCreateFile
+0x26 		 NtCreateIoCompletion
+0x27 		 NtCreateJobObject
+0x28 		 NtCreateJobSet
+0x29 		 NtCreateKey
+0x2A 		 NtCreateMailslotFile
+0x2B 		 NtCreateMutant
+0x2C 		 NtCreateNamedPipeFile
+0x2D 		 NtCreatePagingFile
+0x2E 		 NtCreatePort
+0x2F 		 NtCreateProcess
+0x30 		 NtCreateProcessEx
+0x31 		 NtCreateProfile
+0x32 		 NtCreateSection
+0x33 		 NtCreateSemaphore
+0x34 		 NtCreateSymbolicLinkObject
+0x35 		 NtCreateThread
+0x36 		 NtCreateTimer
+0x37 		 NtCreateToken
+0x38 		 NtCreateWaitablePort
+0x39 		 NtDebugActiveProcess
+0x3A 		 NtDebugContinue
+0x3B 		 NtDelayExecution
+0x3C 		 NtDeleteAtom
+0x3D 		 NtDeleteBootEntry
+0x3E 		 NtDeleteFile
+0x3F 		 NtDeleteKey
+0x40 		 NtDeleteObjectAuditAlarm
+0x41 		 NtDeleteValueKey
+0x42 		 NtDeviceIoControlFile
+0x43 		 NtDisplayString
+0x44 		 NtDuplicateObject
+0x45 		 NtDuplicateToken
+0x46 		 NtEnumerateBootEntries
+0x47 		 NtEnumerateKey
+0x48 		 NtEnumerateSystemEnvironmentValuesEx
+0x49 		 NtEnumerateValueKey
+0x4A 		 NtExtendSection
+0x4B 		 NtFilterToken
+0x4C 		 NtFindAtom
+0x4D 		 NtFlushBuffersFile
+0x4E 		 NtFlushInstructionCache
+0x4F 		 NtFlushKey
+0x50 		 NtFlushVirtualMemory
+0x51 		 NtFlushWriteBuffer
+0x52 		 NtFreeUserPhysicalPages
+0x53 		 NtFreeVirtualMemory
+0x54 		 NtFsControlFile
+0x55 		 NtGetContextThread
+0x56 		 NtGetDevicePowerState
+0x57 		 NtGetPlugPlayEvent
+0x58 		 NtGetWriteWatch
+0x59 		 NtImpersonateAnonymousToken
+0x5A 		 NtImpersonateClientOfPort
+0x5B 		 NtImpersonateThread
+0x5C 		 NtInitializeRegistry
+0x5D 		 NtInitiatePowerAction
+0x5E 		 NtIsProcessInJob
+0x5F 		 NtIsSystemResumeAutomatic
+0x60 		 NtListenPort
+0x61 		 NtLoadDriver
+0x62 		 NtLoadKey
+0x63 		 NtLoadKey2
+0x64 		 NtLockFile
+0x65 		 NtLockProductActivationKeys
+0x66 		 NtLockRegistryKey
+0x67 		 NtLockVirtualMemory
+0x68 		 NtMakePermanentObject
+0x69 		 NtMakeTemporaryObject
+0x6A 		 NtMapUserPhysicalPages
+0x6B 		 NtMapUserPhysicalPagesScatter
+0x6C 		 NtMapViewOfSection
+0x6D 		 NtModifyBootEntry
+0x6E 		 NtNotifyChangeDirectoryFile
+0x6F 		 NtNotifyChangeKey
+0x70 		 NtNotifyChangeMultipleKeys
+0x71 		 NtOpenDirectoryObject
+0x72 		 NtOpenEvent
+0x73 		 NtOpenEventPair
+0x74 		 NtOpenFile
+0x75 		 NtOpenIoCompletion
+0x76 		 NtOpenJobObject
+0x77 		 NtOpenKey
+0x78 		 NtOpenMutant
+0x79 		 NtOpenObjectAuditAlarm
+0x7A 		 NtOpenProcess
+0x7B 		 NtOpenProcessToken
+0x7C 		 NtOpenProcessTokenEx
+0x7D 		 NtOpenSection
+0x7E 		 NtOpenSemaphore
+0x7F 		 NtOpenSymbolicLinkObject
+0x80 		 NtOpenThread
+0x81 		 NtOpenThreadToken
+0x82 		 NtOpenThreadTokenEx
+0x83 		 NtOpenTimer
+0x84 		 NtPlugPlayControl
+0x85 		 NtPowerInformation
+0x86 		 NtPrivilegeCheck
+0x87 		 NtPrivilegeObjectAuditAlarm
+0x88 		 NtPrivilegedServiceAuditAlarm
+0x89 		 NtProtectVirtualMemory
+0x8A 		 NtPulseEvent
+0x8B 		 NtQueryAttributesFile
+0x8C 		 NtQueryBootEntryOrder
+0x8D 		 NtQueryBootOptions
+0x8E 		 NtQueryDebugFilterState
+0x8F 		 NtQueryDefaultLocale
+0x90 		 NtQueryDefaultUILanguage
+0x91 		 NtQueryDirectoryFile
+0x92 		 NtQueryDirectoryObject
+0x93 		 NtQueryEaFile
+0x94 		 NtQueryEvent
+0x95 		 NtQueryFullAttributesFile
+0x96 		 NtQueryInformationAtom
+0x97 		 NtQueryInformationFile
+0x98 		 NtQueryInformationJobObject
+0x99 		 NtQueryInformationPort
+0x9A 		 NtQueryInformationProcess
+0x9B 		 NtQueryInformationThread
+0x9C 		 NtQueryInformationToken
+0x9D 		 NtQueryInstallUILanguage
+0x9E 		 NtQueryIntervalProfile
+0x9F 		 NtQueryIoCompletion
+0xA0 		 NtQueryKey
+0xA1 		 NtQueryMultipleValueKey
+0xA2 		 NtQueryMutant
+0xA3 		 NtQueryObject
+0xA4 		 NtQueryOpenSubKeys
+0xA5 		 NtQueryPerformanceCounter
+0xA6 		 NtQueryQuotaInformationFile
+0xA7 		 NtQuerySection
+0xA8 		 NtQuerySecurityObject
+0xA9 		 NtQuerySemaphore
+0xAA 		 NtQuerySymbolicLinkObject
+0xAB 		 NtQuerySystemEnvironmentValue
+0xAC 		 NtQuerySystemEnvironmentValueEx
+0xAD		 RtlGetNativeSystemInformation
+0xAE 		 NtQuerySystemTime
+0xAF 		 NtQueryTimer
+0xB0 		 NtQueryTimerResolution
+0xB1 		 NtQueryValueKey
+0xB2 		 NtQueryVirtualMemory
+0xB3 		 NtQueryVolumeInformationFile
+0xB4 		 NtQueueApcThread
+0xB5 		 NtRaiseException
+0xB6 		 NtRaiseHardError
+0xB7 		 NtReadFile
+0xB8 		 NtReadFileScatter
+0xB9 		 NtReadRequestData
+0xBA 		 NtReadVirtualMemory
+0xBB 		 NtRegisterThreadTerminatePort
+0xBC 		 NtReleaseMutant
+0xBD 		 NtReleaseSemaphore
+0xBE 		 NtRemoveIoCompletion
+0xBF 		 NtRemoveProcessDebug
+0xC0 		 NtRenameKey
+0xC1 		 NtReplaceKey
+0xC2 		 NtReplyPort
+0xC3 		 NtReplyWaitReceivePort
+0xC4 		 NtReplyWaitReceivePortEx
+0xC5 		 NtReplyWaitReplyPort
+0xC6 		 NtRequestDeviceWakeup
+0xC7 		 NtRequestPort
+0xC8 		 NtRequestWaitReplyPort
+0xC9 		 NtRequestWakeupLatency
+0xCA 		 NtResetEvent
+0xCB 		 NtResetWriteWatch
+0xCC 		 NtRestoreKey
+0xCD 		 NtResumeProcess
+0xCE 		 NtResumeThread
+0xCF 		 NtSaveKey
+0xD0 		 NtSaveKeyEx
+0xD1 		 NtSaveMergedKeys
+0xD2 		 NtSecureConnectPort
+0xD3 		 NtSetBootEntryOrder
+0xD4 		 NtSetBootOptions
+0xD5 		 NtSetContextThread
+0xD6 		 NtSetDebugFilterState
+0xD7 		 NtSetDefaultHardErrorPort
+0xD8 		 NtSetDefaultLocale
+0xD9 		 NtSetDefaultUILanguage
+0xDA 		 NtSetEaFile
+0xDB 		 NtSetEvent
+0xDC 		 NtSetEventBoostPriority
+0xDD 		 NtSetHighEventPair
+0xDE 		 NtSetHighWaitLowEventPair
+0xDF 		 NtSetInformationDebugObject
+0xE0 		 NtSetInformationFile
+0xE1 		 NtSetInformationJobObject
+0xE2 		 NtSetInformationKey
+0xE3 		 NtSetInformationObject
+0xE4 		 NtSetInformationProcess
+0xE5 		 NtSetInformationThread
+0xE6 		 NtSetInformationToken
+0xE7 		 NtSetIntervalProfile
+0xE8 		 NtSetIoCompletion
+0xE9 		 NtSetLdtEntries
+0xEA 		 NtSetLowEventPair
+0xEB 		 NtSetLowWaitHighEventPair
+0xEC 		 NtSetQuotaInformationFile
+0xED 		 NtSetSecurityObject
+0xEE 		 NtSetSystemEnvironmentValue
+0xEF 		 NtSetSystemEnvironmentValueEx
+0xF0 		 NtSetSystemInformation
+0xF1 		 NtSetSystemPowerState
+0xF2 		 NtSetSystemTime
+0xF3 		 NtSetThreadExecutionState
+0xF4 		 NtSetTimer
+0xF5 		 NtSetTimerResolution
+0xF6 		 NtSetUuidSeed
+0xF7 		 NtSetValueKey
+0xF8 		 NtSetVolumeInformationFile
+0xF9 		 NtShutdownSystem
+0xFA 		 NtSignalAndWaitForSingleObject
+0xFB 		 NtStartProfile
+0xFC 		 NtStopProfile
+0xFD 		 NtSuspendProcess
+0xFE 		 NtSuspendThread
+0xFF 		 NtSystemDebugControl
+0x100 		 NtTerminateJobObject
+0x101 		 NtTerminateProcess
+0x102 		 NtTerminateThread
+0x103 		 NtTestAlert
+0x104 		 NtTraceEvent
+0x105 		 NtTranslateFilePath
+0x106 		 NtUnloadDriver
+0x107 		 NtUnloadKey
+0x108 		 NtUnloadKeyEx
+0x109 		 NtUnlockFile
+0x10A 		 NtUnlockVirtualMemory
+0x10B 		 NtUnmapViewOfSection
+0x10C 		 NtVdmControl
+0x10D 		 NtWaitForDebugEvent
+0x10E 		 NtWaitForMultipleObjects
+0x10F 		 NtWaitForSingleObject
+0x110 		 NtWaitHighEventPair
+0x111 		 NtWaitLowEventPair
+0x112 		 NtWriteFile
+0x113 		 NtWriteFileGather
+0x114 		 NtWriteRequestData
+0x115 		 NtWriteVirtualMemory
+0x116 		 NtYieldExecution
+0x117 		 NtCreateKeyedEvent
+0x118 		 NtOpenKeyedEvent
+0x119 		 NtReleaseKeyedEvent
+0x11A 		 NtWaitForKeyedEvent
+0x11B 		 NtQueryPortInformationProcess
+
+"""
+
+# 构建映射字典
+mapping = {}
+for line in numbered_funcs.strip().splitlines():
+    parts = line.strip().split()
+    if len(parts) >= 2:
+        num = parts[0]
+        name = parts[1]
+        mapping[name] = num
+
+# 输出结果
+for func in input_funcs:
+    num = mapping.get(func, "N/A")
+    print(f"{num}\t{func}")
---
+++ doc/ntdll/CopyAllDll.bat
@@ -0,0 +1,4 @@
+mkdir System32
+mkdir SysWow64
+copy C:\Windows\System32\*.dll System32\
+copy C:\Windows\SysWow64\*.dll SysWow64\
