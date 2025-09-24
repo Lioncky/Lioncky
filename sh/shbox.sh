41bdc240f1f7af2295dd5097cda89cf097bae938
CMP <2424527764@qq.com>
2025-09-24 23:05:18 +0800

    [bx] socat dep update

--- sh/shbox.sh
+++ sh/shbox.sh
@@ -4,15 +4,16 @@
 #
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
-Green_font="\033[32m" && Red_font="\033[31m" && Font_suffix="\033[0m"
-Info="${Green_font}[Info]${Font_suffix}"
-Error="${Red_font}[Error]${Font_suffix}"
-echo -e "${Green_font}
+ColGreen="\033[32m" && ColRed="\033[31m" && ColNone="\033[0m"
+Info="${ColGreen}[Info]${ColNone}"
+Error="${ColRed}[Error]${ColNone}"
+ERR_NOT_NUM="❌ 输入0-9非法,请检查"
+echo -e "${ColGreen}
 #======================================
-# Project: shbox-2025-0922
-# Version: 0.0.5 
+# Project: shbox-2025-0925
+# Version: 6 
 #======================================
-${Font_suffix}"
+${ColNone}"
 
 check_root(){
 	[[ "`id -u`" != "0" ]] && echo -e "${Error} must be root user !" && exit 1
@@ -21,6 +22,7 @@ check_root(){
 is_nums() {
     local targetnum="$1"
     if ! [[ "$targetnum" =~ ^[0-9]+$ ]]; then
+		echo ${ERR_NOT_NUM}
         return 1 # Failed return ! 0
     fi
     return 0 # Success return 0
@@ -30,13 +32,13 @@ first(){
 	
 	apt update -y
 	apt install  -y git tmux screen nano vim curl net-tools wget sudo proxychains iperf3 lsof conntrack openssl unzip lsb-release jq ca-certificates bash-completion iptables netcat-openbsd && update-ca-certificates
-	bash <(curl -fsSL https://ghfast.top/https://raw.githubusercontent.com/jamespan2012/shbox/main/dependencies/tcping.sh)
-	bash <(curl -fsSL https://ghfast.top/https://raw.githubusercontent.com/jamespan2012/shbox/main/dependencies/bashrc.sh)
-	git clone https://ghfast.top/https://github.com/iiiiiii1/doubi.git
-	git clone https://ghfast.top/https://github.com/hulisang/Port-forwarding.git
-	git clone https://ghfast.top/https://github.com/vpsxb/EasyRealM.git
-	git clone https://ghfast.top/https://github.com/seal0207/EasyRealM.git
-	bash <(curl -fsSL https://ghfast.top/https://raw.githubusercontent.com/P3TERX/script/master/speedtest-cli.sh)
+	bash <(curl -fsSL https://raw.githubusercontent.com/jamespan2012/shbox/main/dependencies/tcping.sh)
+	bash <(curl -fsSL https://raw.githubusercontent.com/jamespan2012/shbox/main/dependencies/bashrc.sh)
+	git clone https://github.com/iiiiiii1/doubi.git
+	git clone https://github.com/hulisang/Port-forwarding.git
+	git clone https://github.com/vpsxb/EasyRealM.git
+	git clone https://github.com/seal0207/EasyRealM.git
+	bash <(curl -fsSL https://raw.githubusercontent.com/P3TERX/script/master/speedtest-cli.sh)
 	apt install python python3-pip -y
 
 }
@@ -71,13 +73,13 @@ set_proxy(){
 
 multi-v2ray(){
 	
-	source <(curl -fsSL https://ghfast.top/https://raw.githubusercontent.com/Jrohy/multi-v2ray/master/v2ray.sh) --zh
+	source <(curl -fsSL https://raw.githubusercontent.com/Jrohy/multi-v2ray/master/v2ray.sh) --zh
 
 }
 
 x-ui(){
 	
-	bash <(curl -Ls https://ghfast.top/https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
+	bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
 
 }
 
@@ -91,36 +93,36 @@ x_ui_install(){
 
 v2ray-docker(){
 	mkdir -p /root/v2ray
-	wget -O /root/v2ray/config.json https://ghfast.top/https://raw.githubusercontent.com/jamespan2012/shbox/main/dependencies/config-v2ray.json
+	wget -O /root/v2ray/config.json https://raw.githubusercontent.com/jamespan2012/shbox/main/dependencies/config-v2ray.json
 	docker run -d --network host --name v2ray --restart=always -v /root/v2ray:/etc/v2ray teddysun/v2ray
 }
 
 xray-docker(){
 	
 	mkdir -p /root/xray
-	wget -O /root/xray/config.json https://ghfast.top/https://raw.githubusercontent.com/jamespan2012/shbox/main/dependencies/config-xray.json
+	wget -O /root/xray/config.json https://raw.githubusercontent.com/jamespan2012/shbox/main/dependencies/config-xray.json
 	docker run -d --network host --name xray --restart=always -v /root/xray:/etc/xray teddysun/xray
 
 }
 
 hc(){
-	bash <(curl -fsSL https://ghfast.top/https://raw.githubusercontent.com/jamespan2012/shbox/main/dependencies/jcnf.sh)
+	bash <(curl -fsSL https://raw.githubusercontent.com/jamespan2012/shbox/main/dependencies/jcnf.sh)
 }
 
 ihc(){
-	bash <(curl -fsSL https://ghfast.top/https://raw.githubusercontent.com/jamespan2012/shbox/main/dependencies/ijcnf.sh)
+	bash <(curl -fsSL https://raw.githubusercontent.com/jamespan2012/shbox/main/dependencies/ijcnf.sh)
 }
 
 tcpx(){
-	bash <(curl -fsSL https://ghfast.top/https://git.io/JYxKU)
+	bash <(curl -fsSL https://git.io/JYxKU)
 }
 
 ddxt(){
-	bash <(curl -fsSL https://ghfast.top/https://raw.githubusercontent.com/hiCasper/Shell/master/AutoReinstall.sh)
+	bash <(curl -fsSL https://raw.githubusercontent.com/hiCasper/Shell/master/AutoReinstall.sh)
 }
 
 nfcheck(){
-	bash <(curl -fsSL https://ghfast.top/https://raw.githubusercontent.com/lmc999/RegionRestrictionCheck/main/check.sh)
+	bash <(curl -fsSL https://raw.githubusercontent.com/lmc999/RegionRestrictionCheck/main/check.sh)
 }
 
 yabs(){
@@ -128,19 +130,19 @@ yabs(){
 }
 
 lemonbench(){
-	curl -fsSL https://ghfast.top/raw.githubusercontent.com/jamespan2012/shbox/main/dependencies/LemonBench.sh | bash -s fast
+	curl -fsSL raw.githubusercontent.com/jamespan2012/shbox/main/dependencies/LemonBench.sh | bash -s fast
 }
 
 superbench(){
-	bash <(curl -fsSL https://ghfast.top/https://raw.githubusercontent.com/jamespan2012/shbox/main/dependencies/superbench.sh)
+	bash <(curl -fsSL https://raw.githubusercontent.com/jamespan2012/shbox/main/dependencies/superbench.sh)
 }
 
 speed(){
-	curl -fsSL https://ghfast.top/https://raw.githubusercontent.com/jamespan2012/shbox/main/dependencies/superbench.sh | bash -s speed
+	curl -fsSL https://raw.githubusercontent.com/jamespan2012/shbox/main/dependencies/superbench.sh | bash -s speed
 }
 
 io(){
-	curl -fsSL https://ghfast.top/https://raw.githubusercontent.com/jamespan2012/shbox/main/dependencies/superbench.sh | bash -s io
+	curl -fsSL https://raw.githubusercontent.com/jamespan2012/shbox/main/dependencies/superbench.sh | bash -s io
 }
 
 ipcheck(){
@@ -149,11 +151,11 @@ ipcheck(){
 }
 
 tz(){
-	bash <(curl -fsSL https://ghfast.top/https://raw.githubusercontent.com/cokemine/ServerStatus-Hotaru/master/status.sh)
+	bash <(curl -fsSL https://raw.githubusercontent.com/cokemine/ServerStatus-Hotaru/master/status.sh)
 }
 
 jg(){
-	bash <(curl -fsSL https://ghfast.top/raw.githubusercontent.com/Aurora-Admin-Panel/deploy/main/install.sh)
+	bash <(curl -fsSL raw.githubusercontent.com/Aurora-Admin-Panel/deploy/main/install.sh)
 }
 
 xd(){
@@ -207,21 +209,26 @@ oneinstack(){
 }
 
 update_debian(){
-	bash <(curl -sSL https://ghfast.top/raw.githubusercontent.com/wikihost-opensource/linux-toolkit/main/system-upgrade/debian.sh)	
+	bash <(curl -sSL raw.githubusercontent.com/wikihost-opensource/linux-toolkit/main/system-upgrade/debian.sh)	
 }
 
 auto_update_bx(){
 	echo "install... /usr/bin/bx" #mv /root/shbox.sh /usr/bin/bx
-	curl -fsSL -k https://ghfast.top/https://raw.githubusercontent.com/Lioncky/Lioncky/refs/heads/main/sh/shbox.sh -o /usr/bin/bx && chmod +x /usr/bin/bx && bx
+	curl -fsSL -k https://raw.githubusercontent.com/Lioncky/Lioncky/refs/heads/main/sh/shbox.sh -o /usr/bin/bx && chmod +x /usr/bin/bx && bx
 }
 x_hihy(){
-	curl -fsSL -k -o /usr/bin/hihy https://ghfast.top/https://raw.githubusercontent.com/Lioncky/Lioncky/refs/heads/main/sh/az/hihy.sh && chmod +x /usr/bin/hihy && hihy
+	curl -fsSL -k -o /usr/bin/hihy https://raw.githubusercontent.com/Lioncky/Lioncky/refs/heads/main/sh/az/hihy.sh && chmod +x /usr/bin/hihy && hihy
 }
 x_aabt(){
-	wget -q --no-check-certificate -O /usr/bin/aabt https://ghfast.top/https://raw.githubusercontent.com/Lioncky/Lioncky/refs/heads/main/sh/az/aabt.sh && chmod +x /usr/bin/aabt && aabt
+	wget -q --no-check-certificate -O /usr/bin/aabt https://raw.githubusercontent.com/Lioncky/Lioncky/refs/heads/main/sh/az/aabt.sh && chmod +x /usr/bin/aabt && aabt
 }
 x_ovpn(){
-	wget --no-check-certificate https://ghfast.top/https://raw.githubusercontent.com/Nyr/openvpn-install/master/openvpn-install.sh -O /usr/bin/ovpn && chmod +x /usr/bin/ovpn && ovpn
+	wget --no-check-certificate https://raw.githubusercontent.com/Nyr/openvpn-install/master/openvpn-install.sh -O /usr/bin/ovpn && chmod +x /usr/bin/ovpn && ovpn
+}
+x_socat(){
+	apt update 
+	apt install ufw 
+	apt install socat -y
 }
 
 ps_ef(){
@@ -235,17 +242,14 @@ ps_ef(){
 
 port_check(){
     read -p "输入要检查的端口: " nums
-    if ! is_nums "$nums"; then
-        echo "❌ 请输入数字端口号"
-        return 1
+    if is_nums "$nums"; then 
+		lsof -i:$nums
     fi
-    lsof -i:$nums
 }
 
 port_to(){
     read -p "输入要转发的本地端口: " nums
-    if ! [[ "$nums" =~ ^[0-9]+$ ]]; then
-        echo "❌ 请输入数字端口号"
+	if ! is_nums "$nums"; then
         return 1
     fi
 
@@ -256,8 +260,7 @@ port_to(){
     fi
 
     read -p "输入目标端口: " targetnum
-    if ! [[ "$targetnum" =~ ^[0-9]+$ ]]; then
-        echo "❌ 请输入数字端口号"
+	if ! is_nums "$targetnum"; then
         return 1
     fi
 
@@ -274,6 +277,28 @@ proc_kill(){
 	pkill $pnames
 	ps -ef | grep $pnames
 }
+x_ufw_off(){
+	read -p "输入要关闭的端口: " targetnum
+	if is_nums "$targetnum"; then
+        ufw deny ${targetnum} # allow 
+    fi
+}
+x_ufw_on(){
+	read -p "输入要放行的端口: " targetnum
+	if is_nums "$targetnum"; then
+		ufw allow ${targetnum} # allow 
+    fi
+}
+x_add_ssh(){
+	read -p "输入要添加的ssh.pub: " ssh_pub
+	echo "$ssh_pub" >> ~/.ssh/authorized_keys
+	echo -e "已添加~ 正在重启<=>sshd..."
+	echo -e "若无效请检查nano /etc/ssh/sshd_config"
+	echo -e "\t PubkeyAuthentication no->yes"
+	echo -e "\t systemctl restart sshd"
+	systemctl restart sshd
+}
+
 x_help(){
 	echo -e "\033[96m\napt install"
 	echo -e "端口占用: lsof -i:5090"
@@ -282,38 +307,46 @@ x_help(){
 	echo -e "curl -x socks5://127.0.0.1:10808 https://ip.eoeg.cc/1.2.3.4"
 	echo -e "\033[33m"
 }
+x_help_private(){
+	echo -e "\tnohup socat TCP-LISTEN:6090,fork TCP:203.55.176.167:33306 > /dev/null 2>&1 &"
+	echo -e "\tnohup /etc/hihy/bin/appS --log-level info -c /root/xia.json client >/dev/null 2>&1 &"
+}
 
 check_root
 
-echo -e "${Info}选择你要使用的功能: \033[95m \033[92m\t0.帮助  \t 00.自我更新 Ya~"
-echo -e "[安装]1.一键Hy\t 2.宝塔aapanel_zh\t 3.OpenVPN\t4.[x-ui]\t5.端口转发socat\t"
-echo -e "\033[94m 6.yabs测试\t 7.全网测速\t 8.读写IO测试\t 9.流媒体测试 \t\n"
-echo -e "\033[95m11.端口占用\t 22.端口转发\t 33.查找进程\t 44.杀死进程\t 99.本机IP(3322)\n"
+echo -e "${Info}选择你要使用的功能: \033[95m \033[92m\t0.帮助 \t 00.自我更新 ~"
+echo -e "[功能]1.端口占用 \t2.端口转发\t 3.查找进程\t 4.杀死进程\t5.禁止端口\t6.放行端口\t8.添加ssh公钥\t9.IP-3322\n"
+echo -e "\033[95m[安装]\n111.一键Hy\t 222.宝塔aapanel_zh\t 333.OpenVPN\t444.[x-ui]\t555.端口转发socat(首次运行)\t"
+echo -e "666.yabs测试\t 777.全网测速\t 888.读写IO测试\t 999.流媒体测试 \t\n"
 
-echo -e "\033[33m61.首次运行\t 62.安装docker\t 63.安装bbr\t 64.魔法上网\t 65.回程路由(TCP)\t 66.回程路由(ICMP)"
-echo -e "\t 68.superbench\t69.lemonbench\t  33.探针安装"
-echo -e "34.本地IP\t 35.极光面板\t 36.闲蛋面板\t 37.DD系统\t 38.建站环境\t 39.升级Debian(自动执行谨慎操作)"
-echo -e "\n\033[93m请选择:\033[0m"
+echo -e "\033[33m34.本地IP\t 35.极光面板\t 36.闲蛋面板\t 37.DD系统\t 38.建站环境\t 39.升级Debian(自动执行谨慎操作)"
+echo -e "61.首次运行\t 62.安装docker\t 63.安装bbr\t 64.魔法上网\t 65.回程路由(TCP)\t 66.回程路由(ICMP)"
+echo -e "68.superbench\t69.lemonbench\t  33.探针安装"
+echo -e "\n\033[94m请选择:\033[0m"
 read -p "" nums
 
-	if	  [[ "${nums}" == "0" ]]; then x_help
-	elif [[ "${nums}" == "1" ]]; then x_hihy 
-	elif [[ "${nums}" == "2" ]]; then x_aabt
-	elif [[ "${nums}" == "3" ]]; then x_ovpn
-	elif [[ "${nums}" == "4" ]]; then x_ui_install
-	elif [[ "${nums}" == "5" ]]; then apt install socat -y
-	elif [[ "${nums}" == "6" ]]; then yabs
-	elif [[ "${nums}" == "7" ]]; then speed
-	elif [[ "${nums}" == "8" ]]; then io
-	elif [[ "${nums}" == "9" ]]; then nfcheck
-
+	if	 [[ "${nums}" == "0" ]]; then x_help
 	elif [[ "${nums}" == "00" ]]; then auto_update_bx
-	elif [[ "${nums}" == "11" ]]; then port_check
-	elif [[ "${nums}" == "22" ]]; then port_to
-	elif [[ "${nums}" == "33" ]]; then ps_ef
-	elif [[ "${nums}" == "44" ]]; then proc_kill
+	elif [[ "${nums}" == "000" ]]; then x_help_private 
+	
+	elif [[ "${nums}" == "1" ]]; then port_check
+	elif [[ "${nums}" == "2" ]]; then port_to
+	elif [[ "${nums}" == "3" ]]; then ps_ef
+	elif [[ "${nums}" == "4" ]]; then proc_kill
+	elif [[ "${nums}" == "5" ]]; then x_ufw_off
+	elif [[ "${nums}" == "6" ]]; then x_ufw_on
+	elif [[ "${nums}" == "8" ]]; then x_add_ssh
+	elif [[ "${nums}" == "9" ]]; then curl http://ip.3322.net  # show ip
 
-	elif [[ "${nums}" == "99" ]]; then curl http://ip.3322.net  # show ip
+	elif [[ "${nums}" == "111" ]]; then x_hihy 
+	elif [[ "${nums}" == "222" ]]; then x_aabt
+	elif [[ "${nums}" == "333" ]]; then x_ovpn
+	elif [[ "${nums}" == "444" ]]; then x_ui_install
+	elif [[ "${nums}" == "555" ]]; then x_socat
+	elif [[ "${nums}" == "666" ]]; then yabs
+	elif [[ "${nums}" == "777" ]]; then speed
+	elif [[ "${nums}" == "888" ]]; then io
+	elif [[ "${nums}" == "999" ]]; then nfcheck
 
 	#首次安装
 	elif [[ "${nums}" == "61" ]]; then first
